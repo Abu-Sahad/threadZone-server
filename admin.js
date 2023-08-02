621682f9d8ab8341063796998e20b1ee727c0148
Abu-Sahad <abusahad603@gmail.com>
2023-08-02 13:46:39 +0600

    users get api create and email match admin or seller api create and admin seller role set on mongodb

--- admin.js
+++ admin.js
@@ -1,15 +1,37 @@
+const { ObjectId } = require('mongodb');
 const client = require('./client');
-const express = require('express'); 
+const express = require('express');
 const adminRouter = express.Router();
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const usersCollection = client.db('threadZone').collection('users')
+
+        adminRouter.route('/users/admin/:id')
+            .patch(async (req, res) => {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) }
+                const updatedDoc = {
+                    $set: {
+                        role: 'admin'
+                    },
+                };
+                const result = await usersCollection.updateOne(filter, updatedDoc);
+                res.send(result)
+            });
+
+        adminRouter.route('/users/admin/:email')
+            .get(async (req, res) => {
+                const email = req.params.email;
+                const query = { email: email }
+                const user = await usersCollection.findOne(query);
+                const result = { admin: user?.role === 'admin' }
+                res.send(result);
+            })
 
 
-             
-         
 
 
 
